11ea574b3e61f214a226a28161b65ea6fb840ae3
grzla <greg.j.lundberg@gmail.com>
2024-09-04 19:16:50 -0400

    Merged useGameState.ts

---
+++ lib/useGameState.ts
@@ -0,0 +1,43 @@
+import React from 'react';
+import { SongParams, GameCat } from '@/types';
+
+// Manage game state
+function useGameState() {
+
+    const [gameNum, setGameNum] = React.useState<number>(0);
+    const [currentRound, setCurrentRound] = React.useState<GameCat>("namethattune");
+    const [songlist, setSonglist] = React.useState<{
+        [key in GameCat]: SongParams[]
+    }>({
+        namethattune: [],
+        decades: []
+    });
+    const [searchResults, setSearchResults] = React.useState<SongParams[]>([]);
+
+    // Optionally, add functions to update the state
+    const resetGame = () => {
+        setCurrentRound("namethattune");
+        setGameNum(49);
+    };
+
+    const queueToSonglist = (song: SongParams, cat: GameCat = "decades") => {
+        setSonglist((prevSonglist) => ({
+            ...prevSonglist,
+            [cat]: [...prevSonglist[cat], song]
+        }));
+    };
+
+    return {
+        gameNum,
+        setGameNum,
+        currentRound,
+        setCurrentRound,
+        songlist,
+        setSonglist,
+        searchResults,
+        setSearchResults,
+        queueToSonglist,
+    };
+}
+
+export default useGameState;
